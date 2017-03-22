34c52ed3fe90ab55800b473cc9a6116ee59dc7c1
Jake Dawkins <dawkinsjh@gmail.com>
2017-03-21 23:42:22 -0400

    removed flow typings

--- src/setArgs.js
+++ src/setArgs.js
@@ -1,11 +1,10 @@
-// @flow
 import Style from "./";
 
 // can be used to update, overwrite, or reset arguemnts anywhere in the chain
 // f: reducer function for argument object
 
 // #setArgs :: Style ~> (a -> a) -> Style
-const setArgs = function(f: Function) {
+const setArgs = function(f) {
   if (typeof f !== "function") {
     throw TypeError(
       `setArgs expects to be called with a function. Actual: ${f}`
