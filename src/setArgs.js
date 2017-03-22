6bd1970c7bf3b948a583cd59b9ece4edbd54853f
Jake Dawkins <dawkinsjh@gmail.com>
2017-03-21 23:15:03 -0400

    added setArgs method

---
+++ src/setArgs.js
@@ -0,0 +1,19 @@
+// @flow
+import Style from "./";
+
+// can be used to update, overwrite, or reset arguemnts anywhere in the chain
+// f: reducer function for argument object
+
+// #setArgs :: Style ~> (a -> a) -> Style
+const setArgs = function(f: Function) {
+  if (typeof f !== "function") {
+    throw TypeError(
+      `setArgs expects to be called with a function. Actual: ${f}`
+    );
+  }
+  return new Style(props => {
+    return this.resolve(f(props));
+  });
+};
+
+export default setArgs;
